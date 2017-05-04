92b4d3671db6eac4c7a115badb9fcc3521294ed8
Michael Hewson <michael@michaelhewson.ca>
2017-05-04 01:00:49 -0400

    Added tests for trait objects

--- src/lib.rs
+++ src/lib.rs
@@ -179,21 +179,54 @@ pub mod nightly {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::Referent;
-//
-//     trait MyTrait {}
-//     derive_referent!(MyTrait);
-//
-//     impl<T: ?Sized> MyTrait for T {}
-//
-//     #[test]
-//     fn test_basic() {
-//         let foo = "0123456789";
-//         let (data, meta) = Referent::disassemble(foo);
-//         unsafe {
-//             *Referent::assemble(data, meta);
-//         }
-//     }
-// }
+#[test]
+fn test_non_generic_trait_object() {
+    trait Foo {
+        fn foo(&self) -> String;
+    }
+
+    derive_referent!(Foo);
+
+    impl Foo for usize {
+        fn foo(&self) -> String {
+            format!("{}", self)
+        }
+    }
+
+    let foo = &(5 as usize) as &Foo;
+    let (data, meta) = Referent::disassemble(foo);
+    let new_foo: &Foo = unsafe {
+        &*Referent::assemble(data, meta)
+    };
+
+    assert_eq!(new_foo.foo(), "5");
+}
+
+#[test]
+fn test_generic_trait_object() {
+    use std::borrow::Cow;
+    use std::borrow::Cow::*;
+
+    trait Generic<'a, T: Clone> {
+        fn value(&'a self) -> Cow<'a, T>;
+    }
+
+    derive_referent!(<'a, T>, Generic<'a, T>);
+
+    impl<'a> Generic<'a, i32> for i32 {
+        fn value(&'a self) -> Cow<'a, i32> {
+            Borrowed(self)
+        }
+    }
+
+    let i = &1 as &Generic<i32>;
+
+    let (data, meta) = Referent::disassemble(i);
+
+    let new_i: &Generic<i32> = unsafe {
+        &*Referent::assemble(data, meta)
+    };
+
+    assert_eq!(new_i.value(), i.value());
+
+}
